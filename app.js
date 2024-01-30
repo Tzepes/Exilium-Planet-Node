6ab13599e2ebaddc1607a8194407931652ba5e69
Tzepes <andreicristian.vlad@gmail.com>
2024-01-30 18:48:42 +0200

    code cleanup

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from 'url';
 import mongoose from "mongoose";
-import ParcelModel from "./models/parcelSchema.js";
-import UserModel from "./models/metamaskAccSchema.js";
-import * as parcelsController from "./controllers/parcelsController.js";
-import * as usersController from "./controllers/usersController.js";
 import parcelRoutes from "./routes/parcelsRoutes.js";
 import userRoutes from "./routes/usersRoutes.js";
 
@@ -29,19 +25,7 @@ app.get("/message", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.get("/api/parcels", parcelsController.getParcels);
-
-// app.get("/api/parcels/:ownerEthAddress", parcelsController.getParcelsByOwner);
-
-// app.put("/api/parcels/:id", parcelsController.updateParcelOwner);
-
 app.use("/api/parcels", parcelRoutes);
 app.use("/api/users", userRoutes);
-
-// app.get("/api/users/:ethAddress", usersController.getUserByEthAdress);
-
-// app.put("/api/users/createUser", usersController.createUser);
-
-//TODO: create routes for parcels and users, implement controllers to them(will have app.use() for them)
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
